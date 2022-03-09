e7fc335e8b3db12c49f0b3bb9583de50a7ff3229
AlexandruSerban142 <alex.serban142@gmail.com>
2022-03-09 10:56:13 +0200

    project 3: implement contracts

--- 3-blockchain-architecture/project/contracts/coffeecore/Ownable.sol
+++ 3-blockchain-architecture/project/contracts/coffeecore/Ownable.sol
@@ -1,14 +1,15 @@
-pragma solidity ^0.4.24;
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.12;
 
 /// Provides basic authorization control
-contract Ownable {
+abstract contract Ownable {
     address private origOwner;
 
     // Define an Event
     event TransferOwnership(address indexed oldOwner, address indexed newOwner);
 
     /// Assign the contract to an owner
-    constructor () internal {
+    constructor() {
         origOwner = msg.sender;
         emit TransferOwnership(address(0), origOwner);
     }
@@ -20,7 +21,7 @@ contract Ownable {
 
     /// Define a function modifier 'onlyOwner'
     modifier onlyOwner() {
-        require(isOwner());
+        require(isOwner(), "Not owner");
         _;
     }
 
@@ -42,7 +43,7 @@ contract Ownable {
 
     /// Define an internal function to transfer ownership
     function _transferOwnership(address newOwner) internal {
-        require(newOwner != address(0));
+        require(newOwner != address(0), "Not address 0");
         emit TransferOwnership(origOwner, newOwner);
         origOwner = newOwner;
     }
