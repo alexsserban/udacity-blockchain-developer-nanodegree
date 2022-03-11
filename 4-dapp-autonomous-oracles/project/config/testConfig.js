0e5b145cc462b1a68b5aff3027aec90141f48aff
AlexandruSerban142 <alex.serban142@gmail.com>
2022-03-11 16:34:11 +0200

    project 4: remove frontend boilerplate

--- 4-dapp-autonomous-oracles/project/config/testConfig.js
+++ 4-dapp-autonomous-oracles/project/config/testConfig.js
@@ -1,10 +1,8 @@
-
 var FlightSuretyApp = artifacts.require("FlightSuretyApp");
 var FlightSuretyData = artifacts.require("FlightSuretyData");
-var BigNumber = require('bignumber.js');
+var BigNumber = require("bignumber.js");
 
-var Config = async function(accounts) {
-    
+var Config = async function (accounts) {
     // These test addresses are useful when you need to add
     // multiple users in test scripts
     let testAddresses = [
@@ -16,9 +14,8 @@ var Config = async function(accounts) {
         "0x6b85cc8f612d5457d49775439335f83e12b8cfde",
         "0xcbd22ff1ded1423fbc24a7af2148745878800024",
         "0xc257274276a4e539741ca11b590b9447b26a8051",
-        "0x2f2899d6d35b1a48a4fbdc93a37a72f264a9fca7"
+        "0x2f2899d6d35b1a48a4fbdc93a37a72f264a9fca7",
     ];
-
 
     let owner = accounts[0];
     let firstAirline = accounts[1];
@@ -26,17 +23,16 @@ var Config = async function(accounts) {
     let flightSuretyData = await FlightSuretyData.new();
     let flightSuretyApp = await FlightSuretyApp.new();
 
-    
     return {
         owner: owner,
         firstAirline: firstAirline,
-        weiMultiple: (new BigNumber(10)).pow(18),
+        weiMultiple: new BigNumber(10).pow(18),
         testAddresses: testAddresses,
         flightSuretyData: flightSuretyData,
-        flightSuretyApp: flightSuretyApp
-    }
-}
+        flightSuretyApp: flightSuretyApp,
+    };
+};
 
 module.exports = {
-    Config: Config
+    Config: Config,
 };
--- 4-dapp-autonomous-oracles/project/truffle.js
+++ 4-dapp-autonomous-oracles/project/truffle.js
@@ -1,19 +1,31 @@
-var HDWalletProvider = require("truffle-hdwallet-provider");
-var mnemonic = "candy maple cake sugar pudding cream honey rich smooth crumble sweet treat";
+require("dotenv").config();
+
+const HDWalletProvider = require("@truffle/hdwallet-provider");
 
 module.exports = {
-  networks: {
-    development: {
-      provider: function() {
-        return new HDWalletProvider(mnemonic, "http://127.0.0.1:8545/", 0, 50);
-      },
-      network_id: '*',
-      gas: 9999999
-    }
-  },
-  compilers: {
-    solc: {
-      version: "^0.4.24"
-    }
-  }
+    networks: {
+        development: {
+            host: "127.0.0.1", // Localhost (default: none)
+            port: 7545, // Standard Ethereum port (default: none)
+            network_id: "*", // Any network (default: none)
+        },
+
+        rinkeby: {
+            provider: () =>
+                new HDWalletProvider({
+                    privateKeys: [process.env.PRIVATE_KEY],
+                    providerOrUrl: `https://rinkeby.infura.io/v3/${process.env.INFURA_ID}`,
+                }),
+            network_id: 4,
+            from: process.env.PUBLIC_KEY,
+        },
+    },
+
+    mocha: {},
+
+    compilers: {
+        solc: {
+            version: "0.8.12",
+        },
+    },
 };
